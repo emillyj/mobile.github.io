27e6078374e4d7f362860fa77b86cf6148b2c915
emillyj <jenifferemilly8044@outlook.com>
2022-04-22 17:14:31 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ self.addEventListener('install', function(event){
     event.waitUntil(
         caches.open(cacheName).then(function (cache){
             cache.addAll([
-                '../mobile.github.Login.html',
-                './manifest.webmanifest',
+                '../mobile.github/',
+                './manifest.json',
                 './login.js',
                 './Login.html',
                 './login.css',
