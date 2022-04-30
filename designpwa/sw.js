8fd32ca8c0ea26220cd01d821e215cdac7b16498
emillyj <jenifferemilly8044@outlook.com>
2022-04-30 16:07:35 -0300

    mobile

--- designpwa/sw.js
+++ designpwa/sw.js
@@ -6,9 +6,54 @@ toolbox.precache([
   'js/BluetoothConnection.js',
   'js/companion.js',
   'js/main.js',
-  'index.html',
-  'tutorial.3.html',
-  'css/tutorial.3.css'
+  
+  'css/adicionarpet.css',
+  'css/atividades.css',
+  'css/batimentos.css',
+  'css/batimentos02.html'
+  'css/bluetooth.css',
+  'css/cadastro.css',
+  'css/editar.css',
+  'css/home.css',
+  'css/index.css',
+  'css/inicio.css',
+  'css/login.css',
+  'css/perfildopet.css',
+  'css/perfildopet02.css',
+  'css/termos.css',
+  'css/tutorial1.css',
+  'css/tutorial2.css',
+  'css/tutorial4.css',
+  'css/tutorial5.css',
+  'css/tutorialfinal.css',
+
+
+  'adicionarpet.html',
+  'atividades.html',
+  'batimentos.html.',
+  'batimentos02.html',
+  'bluetooth.html.',
+  'cadastro.html.',
+  'editar.html.',
+  'editar02.html'
+  'home.html.',
+  'index.html.',
+  'inicio.html.',
+  'login.html',
+  'perfildopet.html',
+  'perfildopet02.html',
+  'termos.html',
+  'tutorial1.html',
+  'tutorial2.html',
+  'tutorial4.html',
+  'tutorial5.html',
+  'tutorialfinal.html',
+
+
+  
+  
+
+
 ]);
 
 toolbox.router.default = toolbox.networkFirst;
